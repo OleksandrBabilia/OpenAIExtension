d94a83b2d06808000de7e5f0ebbc9339d4484afa
Alex Babilia <djesssash1708@gmail.com>
2023-06-20 16:36:26 +0300

    Added: Backend Request Process func

--- backend/openai/views.py
+++ backend/openai/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
+import json
+
+def index(request):
+    return HttpResponse("Hello, world!")
+
+def get_openai_summary(request):
+    topic = request.GET.get('topic', None)
+    
+    print(f'topic: {topic}')
+    
+    return JsonResponse({
+        'ok': True,
+        'data': topic
+    })
