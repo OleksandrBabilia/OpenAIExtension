09e28fe6b157ee4a4c04833a8c6516dc79815f2f
Alex Babilia <djesssash1708@gmail.com>
2023-06-23 15:12:36 +0300

    Added: Auth Func

---
+++ backend/authentication.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup as BS
+
+def authenticate(username: str, password: str, login_url: str) -> None:
+    session = requests.Session()
+
+    response = session.get(login_url)
+    soup = BS(response.content, 'html.parser')
+    logintoken = soup.find('input', {'name': 'logintoken'}).get('value')
+
+    login_data = {
+        'logintoken': logintoken,
+        'username': username,
+        'password': password
+    }
+
+    response = session.post(login_url, data=login_data)
+
+    if response.status_code == 200:
+        print('Authentication successful.')
+        return session
+    else:
+        print('Authentication failed.')
+        return None
