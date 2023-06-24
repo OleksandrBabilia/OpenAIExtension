649b0d107e7823323b5e0f9a3688291ab8812a82
Alex Babilia <djesssash1708@gmail.com>
2023-06-24 15:53:51 +0300

    Added: Page URL, Password, Username to server request

--- frontend/popup.js
+++ frontend/popup.js
@@ -1,20 +1,25 @@
-document.getElementById('sendrequest').addEventListener('click', function() {
-    var apiUrl = 'http://127.0.0.1:8000/api/get_openai_summary/?topic=Hello';  // Replace with your API endpoint URL
+document.getElementById('send-request').addEventListener('click', function() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        var activeTab = tabs[0];
+        var apiUrl = 'http://127.0.0.1:8000/api/get_openai_summary/?username=' + encodeURIComponent(username) + '&password=' + encodeURIComponent(password) + '&page_url=' + encodeURIComponent(activeTab.url);
 
         fetch(apiUrl)
-        .then(function(response) {
-            if (!response.ok) {
-            throw new Error('Error: ' + response.status);
-            }
-            return response.json();
-        })
-        .then(function(data) {
-            var resultElement = document.getElementById('result');
-            resultElement.textContent = data.data;  
-            console.log(data);  
-        })
-        .catch(function(error) {
-            console.log('Request failed:', error);
-        });
-
+            .then(function(response) {
+                if (!response.ok) {
+                    throw new Error('Error: ' + response.status);
+                }
+                return response.json();
+            })
+            .then(function(data) {
+                var resultElement = document.getElementById('result');
+                resultElement.textContent = data.data;
+                console.log(data);
+            })
+            .catch(function(error) {
+                console.log('Request failed:', error);
+            });
+    });
 });
