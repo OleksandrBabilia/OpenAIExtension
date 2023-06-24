a5e5d63a1328ed20da07168953b21db9406e5846
Alex Babilia <djesssash1708@gmail.com>
2023-06-24 15:48:03 +0300

    Added: Questions Parser

---
+++ backend/data_parser.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup as BS
+
+from authentication import authenticate
+
+def parse(username: str, password:str, page_url: str) -> list[str]:
+    login_url = 'https://vns.lpnu.ua/login/index.php'
+
+    session = authenticate(username, password, login_url)
+
+    if session is None:
+        return None
+
+    response = session.get(page_url)
+
+    if response.status_code == 200:
+        result = []
+        
+        soup = BS(response.content, 'html.parser')
+        question_divs = soup.find_all(class_="qtext")
+        answer_divs = soup.find_all(class_="answer")
+        
+        if not(question_divs or answer_divs):
+            print('Failed to retrieve questions or answers.')
+            return None
+        
+        for question_div, answer_div in zip(question_divs, answer_divs):
+            result.append(f"Питання: {question_div.text.strip()}\n Відповіді: {answer_div.text.strip()}")
+
+        return result
+    
+    else:
+        print('Failed to retrieve page for parsing.')
+        return None
