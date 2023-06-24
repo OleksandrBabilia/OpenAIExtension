5c687327f8d1f7d2868cf8fbbbb6e791b4faea32
Alex Babilia <djesssash1708@gmail.com>
2023-06-24 15:49:59 +0300

    Added: Main EndPoint

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,27 @@
 from fastapi import FastAPI
 
+from data_parser import parse
+from chatgpt import generate_response
+
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
 @app.get("/api/get_openai_summary/")
-async def root(topic: str):
-    print(topic)
-    return {"data": f"{topic} back"}
+async def root(username: str, password: str, page_url: str) -> list[str]:
+    result = []
+    questions = parse(username, password, page_url)
+    
+    if questions is None:
+        return {
+            'success': False,
+        }
+        
+    for i, question in enumerate(questions):
+        response = generate_response(question)
+        result.append(f"{i+1}. {response}")
+        
+    return {
+            'success': False,
+            'data': result
+        }
+    
