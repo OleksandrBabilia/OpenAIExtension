25a90f08f521a1f3dbebc80ab9d55f9471b36c0a
Alex Babilia <djesssash1708@gmail.com>
2023-06-27 16:47:54 +0300

    Fixed: Env Variables

--- backend/chatgpt.py
+++ backend/chatgpt.py
@@ -1,13 +1,16 @@
 import openai
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
+load_dotenv(dotenv_path)
 
 openai.api_key = os.environ['API_KEY']
 
-def generate_response(question):
+def generate_response(question: str):
     response = openai.Completion.create(
         engine='text-davinci-003',
         prompt=question,
-        max_tokens=5 
+        max_tokens=40 
     )
     return response.choices[0].text.strip()
