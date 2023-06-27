5db55a13a8176c0de2e26c8c204eb68e5ae48078
Alex Babilia <djesssash1708@gmail.com>
2023-06-27 16:50:46 +0300

    Added: Question String  Formatting

--- backend/main.py
+++ backend/main.py
@@ -7,21 +7,21 @@ app = FastAPI()
 
 
 @app.get("/api/get_openai_summary/")
-async def root(username: str, password: str, page_url: str) -> list[str]:
+async def root(username: str, password: str, page_url: str) :
+    
     result = []
     questions = parse(username, password, page_url)
-    
     if questions is None:
         return {
             'success': False,
         }
         
     for i, question in enumerate(questions):
-        response = generate_response(question)
+        formatted_question = question.replace('\n', '').replace('\r', '')
+        response = generate_response(formatted_question)
         result.append(f"{i+1}. {response}")
         
     return {
-            'success': False,
+            'success': True,
             'data': result
         }
-    
